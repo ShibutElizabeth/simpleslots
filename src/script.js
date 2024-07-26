66948b97ba542c53ab80982d784226389c3fcacf
ShibutElizabeth <ellis.shybut@mail.ru>
2024-07-26 23:49:34 +0200

    feat: latest

--- src/script.js
+++ src/script.js
@@ -1,92 +1,60 @@
 
-/**
- * Setup
- */
-const //debugEl = document.getElementById('debug'),
-			// Mapping of indexes to icons: start from banana in middle of initial position and then upwards
-			iconMap = [ "one", "eight", "zero", "four", "seven", "two", "three", "six", "nine", "five"],
-			// Width of the icons
-			icon_width = 100,	
-			// Height of one icon in the strip
-			icon_height = 100,	
-			// Number of icons in the strip
-			num_icons = 10,	
-			// Max-speed in ms for animating one icon down
-			time_per_icon = 100,
-			// Holds icon indexes
-			indexes = [0, 0, 0];
+// Mapping of indexes to icons on the strip: start from the 2nd item and then upwards
+const iconMap = [ 
+	"one", "eight", "zero", "four", "seven", 
+	"two", "three", "six", "nine", "five"
+];
+const iconWidth = 100; // Width of the icons
+const iconHeight = 100; // Height of one icon in the strip
+const numOfIcons = 10; // Number of icons in the strip
+const iconSpeed = 100; // Max-speed in ms for animating one icon down
+const indexes = [0, 0, 0]; // Holds icon indexes
+// const debugEl = document.querySelector('#debug');
+
+const numOfSpins = 5; // Number of the strip spins
+const hardcodedValue = 12; // 10+2 (numOfIcons + "zero" icon index)
 
 
-/** 
- * Roll one reel
- */
 const roll = (reel, offset = 0) => {
-	// Minimum of 2 + the reel offset rounds
-	const delta = offset === 0 ?  (offset + 5) * num_icons + 12 : (offset + 5) * num_icons + Math.round(Math.random() * num_icons); 
-	//offset === 10 ?  (offset + 5) * num_icons + 11 :
-	// Return promise so we can wait for all reels to finish
+	//target icon calculations
+	const delta = offset === 0 
+		? (offset + numOfSpins) * numOfIcons + hardcodedValue 
+		: (offset + numOfSpins) * numOfIcons + Math.round(Math.random() * numOfIcons); 
+
 	return new Promise((resolve, reject) => {
 		
-		const style = getComputedStyle(reel),
-					// Current background position
-					backgroundPositionY = parseFloat(style["background-position-y"]),
-					// Target background position
-					targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
-					// Normalized background position, for reset
-					normTargetBackgroundPositionY = targetBackgroundPositionY%(num_icons * icon_height);
+		const style = getComputedStyle(reel);
+		const backgroundPositionY = parseFloat(style["background-position-y"]);
+		const targetBackgroundPositionY = backgroundPositionY + delta * iconHeight;
+		const normTargetBackgroundPositionY = targetBackgroundPositionY%(numOfIcons * iconHeight);
 		
-		// Delay animation with timeout, for some reason a delay in the animation property causes stutter
 		setTimeout(() => { 
-			// Set transition properties ==> https://cubic-bezier.com/#.41,-0.01,.63,1.09
-			reel.style.transition = `background-position-y ${(8 + 1 * delta) * time_per_icon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
-			// Set background position
-			reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
+			reel.style.transition = `background-position-y ${(8 + 1 * delta) * iconSpeed}ms cubic-bezier(.41,-0.01,.63,1.09)`;
+			reel.style.backgroundPositionY = `${backgroundPositionY + delta * iconHeight}px`;
 		}, offset * 150);
 			
-		// After animation
 		setTimeout(() => {
-			// Reset position, so that it doesn't get higher without limit
 			reel.style.transition = `none`;
 			reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
-			// Resolve this promise
-			resolve(delta%num_icons);
-		}, (8 + 1 * delta) * time_per_icon + offset * 150);
-		
+			resolve(delta % numOfIcons);
+		}, (8 + 1 * delta) * iconSpeed + offset * 150);
 	});
 };
 
+rollAll = () => {
+	const reelsList = document.querySelectorAll('.reel');
 
-/**
- * Roll all reels, when promise resolves roll again
- */
-function rollAll() {
-	
-	// debugEl.textContent = 'rolling...';
-	
-	const reelsList = document.querySelectorAll('.slots > .reel');
-	
 	Promise
-		
-		// Activate each reel, must convert NodeList to Array for this with spread operator
-		.all( [...reelsList].map((reel, i) => roll(reel, i)) )	
-		
-		// When all reels done animating (all promises solve)
+		.all([...reelsList].map((reel, i) => roll(reel, i)))	
 		.then((deltas) => {
 			// add up indexes
-			deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta)%(num_icons));
+			deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta)%(numOfIcons));
 			// debugEl.textContent = indexes.map((i) => iconMap[i]).join(' - ');
-			// console.log(deltas);
-			// Win conditions
-			// if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
-			// 	const winCls = indexes[0] == indexes[2] ? "win2" : "win1";
-			// 	document.querySelector(".slots").classList.add(winCls);
-			// 	setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000)
-			// }
-		
-			// Again!
-			// setTimeout(rollAll, 3000);
 		});
 };
 
-// Kickoff
+// setTimeout start
 setTimeout(rollAll, 1000);
+
+// EventListener start
+// document.body.addEventListener(('click'), rollAll, {once: true});
